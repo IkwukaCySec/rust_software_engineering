bcec666dd392d57766cce118509061ff7601c67d
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-03-08 00:16:45 -0500

    Create variable project and debug with cargo

---
+++ 0x01-programming_concepts/variable/target/debug/deps/variable-5df5d69b8b34f26d.d
@@ -0,0 +1,5 @@
+/home/kali/CySec_repos/rust_software_engineering/0x01-programming_concepts/variable/target/debug/deps/variable-5df5d69b8b34f26d.d: src/main.rs
+
+/home/kali/CySec_repos/rust_software_engineering/0x01-programming_concepts/variable/target/debug/deps/variable-5df5d69b8b34f26d: src/main.rs
+
+src/main.rs:
